cd2a6d7076a39a1756167aa1fab1c63954f28508
Henrique <Henrique@moraes>
2015-02-06 04:10:48 -0200

    Tutorial parte 6

--- module/Contato/Module.php
+++ module/Contato/Module.php
@@ -3,11 +3,17 @@ namespace Contato;
 
 class Module
 {
+    /**
+     * include de arquivo para outras configuracoes desse modulo
+     */    
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
     }
 
+    /**
+     * autoloader para nosso modulo
+     */    
     public function getAutoloaderConfig()
     {
         return array(
@@ -18,4 +24,22 @@ class Module
             ),
         );
     }
+    
+    /**
+     * Register View Helper
+     */      
+    public function getViewHelperConfig()
+    {
+        return array(
+            # registrar View Helper com injecao de dependecia
+            'factories' => array(
+                'menuAtivo'  => function($sm) {
+                    return new View\Helper\MenuAtivo($sm->getServiceLocator()->get('Request'));
+                },
+                'message' => function($sm) {
+                    return new View\Helper\Message($sm->getServiceLocator()->get('ControllerPluginManager')->get('flashmessenger'));
+                },
+            )
+        );
+    }    
 }
